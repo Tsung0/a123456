86944e7b910a8b1122a3c87a85a085fde508932e
Tsung0 <114997511+Tsung0@users.noreply.github.com>
2022-10-06 17:03:03 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,96 +1,66 @@
-let x = 0
-sensors.DDMmotor(
-AnalogPin.P15,
-0,
-AnalogPin.P16,
-0
-)
-basic.forever(function () {
-    x = pins.digitalReadPin(DigitalPin.P1)
-    if (x == 0) {
-        for (let index = 0; index < 11; index++) {
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            music.playTone(587, music.beat(BeatFraction.Half))
-            basic.pause(300)
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            music.playTone(587, music.beat(BeatFraction.Half))
-            basic.pause(300)
-        }
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-    }
+function right () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    120
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    120
+    )
+}
+function left () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    120
+    )
     sensors.DDMmotor(
     AnalogPin.P15,
     0,
     AnalogPin.P16,
-    99
+    120
     )
-    basic.pause(500)
+}
+function stop () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
     sensors.DDMmotor(
     AnalogPin.P15,
     0,
     AnalogPin.P16,
     0
     )
-})
+}
+function go () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    120
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    120
+    )
+}
+stop()
 basic.forever(function () {
-    x = pins.digitalReadPin(DigitalPin.P1)
-    if (x == 0) {
-        basic.pause(1000)
-        sensors.DDMmotor(
-        AnalogPin.P15,
-        1,
-        AnalogPin.P16,
-        54
-        )
-        basic.pause(400)
-        sensors.DDMmotor(
-        AnalogPin.P0,
-        0,
-        AnalogPin.P0,
-        0
-        )
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                . # . . .
-                # . . . .
-                # # # # .
-                # . . . .
-                . # . . .
-                `)
-            basic.showLeds(`
-                # . . . .
-                . . . . .
-                # # # . #
-                . . . . .
-                # . . . .
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . #
-                # # . # #
-                . . . . #
-                . . . . .
-                `)
-            basic.showLeds(`
-                . . . . #
-                . . . # .
-                # . # # #
-                . . . # .
-                . . . . #
-                `)
-            basic.showLeds(`
-                . . . # .
-                . . # . .
-                . # # # #
-                . . # . .
-                . . . # .
-                `)
-            basic.showArrow(ArrowNames.West)
-        }
-    } else {
-        basic.clearScreen()
+    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 0) {
+        go()
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0) {
+        right()
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 1) {
+        left()
     }
 })
